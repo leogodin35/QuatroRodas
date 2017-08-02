d9a81c05b43d1333a0b61dccb6a5b2063ec75d53
leogodin35 <godinho.leo@gmail.com>
2017-08-02 15:36:19 -0300

    links quebrados corrigidos

--- wp-content/themes/wp-teste4rodas/index.php
+++ wp-content/themes/wp-teste4rodas/index.php
@@ -6,23 +6,23 @@
         <section id="principal">
           <div class="row painel padding-zero">
             <div class="col-sm-6 col-xs-12 figura-principal padding-zero">
-              <a href="<?php the_permalink('http://leonardogodinho.16mb.com/'); ?>"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/carros_grandes.jpg" alt="carros_grandes"></a>
+              <a href="http://leonardogodinho.16mb.com/bmw-confirma-novos-x1-e-serie-3-no-brasil/"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/carros_grandes.jpg" alt="carros_grandes"></a>
             </div>
             <div class="col-sm-6 col-xs-12 figura-geral padding-zero">
               <div class="row figura-geral-linha padding-zero">
                 <div class="col-sm-6 col-xs-12 padding-zero">
-                  <a href="<?php the_permalink('http://leonardogodinho.16mb.com/'); ?>"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/fabricantes.jpg" alt="fabricantes"></a>
+                  <a href="http://leonardogodinho.16mb.com/frankfurt-kia-revela-novo-sportage/"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/fabricantes.jpg" alt="fabricantes"></a>
                 </div>
                 <div class="col-sm-6 col-xs-12 padding-zero">
-                  <a href="<?php the_permalink('http://leonardogodinho.16mb.com/'); ?>"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/publicidade.jpg" alt="publicidade"></a>
+                  <a href="http://leonardogodinho.16mb.com/"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/publicidade.jpg" alt="publicidade"></a>
                 </div>
               </div>
               <div class="row figura-geral-linha padding-zero">
                 <div class="col-sm-6 col-xs-12 padding-zero">
-                  <a href="<?php the_permalink('http://leonardogodinho.16mb.com/'); ?>"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/fabricantes2.jpg" alt="fabricantes2"></a>
+                  <a href="http://leonardogodinho.16mb.com/frankfurt-porsche-misson-e-da-um-show/"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/fabricantes2.jpg" alt="fabricantes2"></a>
                 </div>
                 <div class="col-sm-6 col-xs-12 padding-zero">
-                  <a href="<?php the_permalink('http://leonardogodinho.16mb.com/'); ?>"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/promovido.jpg" alt="promovido"></a>
+                  <a href="http://leonardogodinho.16mb.com/nova-geracao-do-volkswagen-tiguan/"><img class="img-responsive img-4rodas" src="<?php echo(get_template_directory_uri()); ?>/imagens/promovido.jpg" alt="promovido"></a>
                 </div>
               </div>
             </div>
